01d8933752ca7306afe96f610b37a69d86018058
ahacs <583937325@qq.com>
2020-02-16 16:30:29 +0800

    ss

--- SpringBootDemo/src/main/java/com/calculatedfundemo/SpringBootDemoApplication.java
+++ SpringBootDemo/src/main/java/com/calculatedfundemo/SpringBootDemoApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @ServletComponentScan
+// @ComponentScan(basePackages = {
+// "com.calculatedfundemo"//"com.calculatedfun.component.*",
+// "com.calculatedfun.logAop",
+// })"com.kaifantech.component.*" })
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
-
 }
